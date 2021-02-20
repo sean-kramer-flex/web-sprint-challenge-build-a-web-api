cc4a292f75ee7788dfc5b53f73d5a3e8fd189b4d
Sean Kramer <seandominickramer@gmail.com>
2021-02-19 20:07:43 -0500

    Add logger middleware

--- api/server.js
+++ api/server.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const server = express();
+const logger = require('../middleware/logger')
 const projectsRouter = require('./projects/projects-router')
 
 server.use(express.json())
+server.use(logger)
 
 server.get('/welcome', (req, res) => {
   res.send('<h2>Welcome! Server is working!</h2>')
