df7e52cb804085d1a1ceeb0badd28eb1b89e8429
Sean Kramer <seandominickramer@gmail.com>
2021-02-19 21:33:18 -0500

    Add endpoint to get action by id

--- api/actions/actions-router.js
+++ api/actions/actions-router.js
@@ -13,5 +13,15 @@ res.json(actionData)
   }
 })
 
+//get action by id
+router.get('/:id', async(req, res, next) => {
+  try {
+const action = await actions.get(req.params.id)
+res.json(action)
+  } catch(err) {
+    next(err)
+  }
+})
+
 
 module.exports = router
