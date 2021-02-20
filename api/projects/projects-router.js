ec28ab8e27d3cae72d85683f85fa4a6aaba514e4
Sean Kramer <seandominickramer@gmail.com>
2021-02-19 19:59:05 -0500

    Add endpoint to get projects

--- api/projects/projects-router.js
+++ api/projects/projects-router.js
@@ -1,1 +1,18 @@
-// Write your "projects" router here!
+const express = require('express')
+const projects = require('./projects-model')
+
+const router = express.Router()
+
+router.get('/', async (req, res, next) => {
+  try {
+    const projectsData = await projects.get()
+    res.status(200).json(projectsData)
+  } catch(err) {
+    next(err)
+  }
+
+}
+)
+
+
+module.exports = router
--- api/server.js
+++ api/server.js
@@ -1,8 +1,13 @@
 const express = require('express');
 const server = express();
+const projectsRouter = require('./projects/projects-router')
+
+server.use(express.json())
 
 server.get('/welcome', (req, res) => {
   res.send('<h2>Welcome! Server is working!</h2>')
 })
+
+server.use('/projects', projectsRouter)
 
 module.exports = server;
